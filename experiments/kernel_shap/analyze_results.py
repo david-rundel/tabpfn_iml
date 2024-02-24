f2b3d20c68ec82f511e5269bebc109a425065efb
davidrundel <david.rundel@campus.lmu.de>
2024-02-24 08:21:03 +0100

    Results analysis

--- experiments/kernel_shap/analyze_results.py
+++ experiments/kernel_shap/analyze_results.py
@@ -4,131 +4,177 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 import seaborn as sns
 import pickle
+import openml
 
-experiment_results_path= 'experiments/kernel_shap/results/kernel_shap_900_20240222_012027.pkl'
+experiment_770_results_path= 'experiments/kernel_shap/results/kernel_shap_770_20240221_234505.pkl'
+experiment_819_results_path= 'experiments/kernel_shap/results/kernel_shap_819_20240222_101704.pkl'
+experiment_900_results_path= 'experiments/kernel_shap/results/kernel_shap_900_20240222_012027.pkl'
 
-with open(experiment_results_path, 'rb') as file:
-    exp_results = pickle.load(file)
 
-rectify_indices= True
+experiment_results_paths= [experiment_770_results_path,
+                           experiment_819_results_path,
+                           experiment_900_results_path]
+
+exp_results= {}
+for experiment_results_path in experiment_results_paths:
+    with open(experiment_results_path, 'rb') as file:
+        openml_id= int(experiment_results_path.split("_")[-3])
+        exp_results[openml_id] = pickle.load(file)
+
+rectify_indices= False
 if rectify_indices:
-    loss_exact_marg_runs= exp_results["loss_exact_marginalization_per_run"]
-    loss_appr_marg_runs= exp_results["loss_appr_marginalization_per_run"]
-    max_s= exp_results["experiment_hyperparameters"]["max_s"]
-    runs= exp_results["experiment_hyperparameters"]["runs"]
+    for openml_id, temp_exp_results in exp_results.items():
+        temp_loss_exact_marg_runs= temp_exp_results["loss_exact_marginalization_per_run"]
+        temp_loss_appr_marg_runs= temp_exp_results["loss_appr_marginalization_per_run"]
+        temp_max_s= temp_exp_results["experiment_hyperparameters"]["max_s"]
+        temp_runs= temp_exp_results["experiment_hyperparameters"]["runs"]
+
+        appr_cols= list(range(1, temp_max_s+1))
+        exact_col= [-1]
+        temp_exp_results["loss_exact_marginalization_mean"]= pd.DataFrame(np.concatenate([temp_loss_exact_marg_runs[i] for i in range(temp_runs)], axis=1).mean(axis=-1), columns= exact_col, index= temp_loss_exact_marg_runs[0].index)
+        temp_exp_results["loss_appr_marginalization_mean"]= pd.DataFrame(np.stack([temp_loss_appr_marg_runs[i] for i in range(temp_runs)]).mean(axis=0), columns= appr_cols, index= temp_loss_appr_marg_runs[0].index)
+        temp_exp_results["loss_exact_marginalization_std"]= pd.DataFrame(np.concatenate([temp_loss_exact_marg_runs[i] for i in range(temp_runs)], axis=1).std(axis=-1), columns= exact_col, index= temp_loss_exact_marg_runs[0].index)
+        temp_exp_results["loss_appr_marginalization_std"]= pd.DataFrame(np.stack([temp_loss_appr_marg_runs[i] for i in range(temp_runs)]).std(axis=0), columns= appr_cols, index= temp_loss_appr_marg_runs[0].index)
+
+        exp_results[openml_id]= temp_exp_results
+
+cmap_original = plt.get_cmap('magma')
+cmap_inverted = ListedColormap(cmap_original.colors[::-1])
+
+def plot_loss_per_ML(exp_results, save):
+    plot_dfs= {}
+    for openml_id, temp_exp_results in exp_results.items():
+        plot_dfs[openml_id]= temp_exp_results["loss_appr_marginalization_mean"].copy()
+        plot_dfs[openml_id][-1]= temp_exp_results["loss_exact_marginalization_mean"].copy()
+
+    fig, axs = plt.subplots(1, 3, figsize=(12, 4), sharey=True)
+
+    i=0
+    for openml_id, plot_df in plot_dfs.items():
+        x, y = np.meshgrid(plot_df.columns, plot_df.index)
+        x_flat = x.flatten()
+        y_flat = y.flatten()
+        values = plot_df.values.flatten()
+
+        scatter = axs[i].scatter(x_flat, y_flat, c=values, cmap=cmap_inverted, s=15)
+        axs[i].set_title(f'{str(openml.datasets.get_dataset(openml_id).name)} dataset')
+        axs[i].set_xlabel('L')
+        axs[i].set_ylabel('M')
+        temp_xticks= [-1, 1]
+        temp_xticks.extend(list(np.arange(5, max(plot_df.columns)+1, 5)))
+        axs[i].set_xticks(temp_xticks)
+        axs[i].axvline(x=0, color='gray', linestyle='-', linewidth=0.7)
+        i+=1
+    cbar_ax = fig.add_axes([0.92, 0.15, 0.02, 0.7])
+    cbar = fig.colorbar(scatter, cax=cbar_ax, label='Loss')
+
+    #fig.suptitle('Mean loss of exact and approximate feature marginalization over 10 runs depending on M and L.')
+
+    if save:
+        plt.savefig('experiments/kernel_shap/results/Loss_M_L.pdf', bbox_inches='tight')  
+    else:
+        plt.show()
+
+plot_loss_per_ML(exp_results, True)
 
 
-    appr_cols= list(range(1, max_s+1))
-    exact_col= [-1]
-    exp_results["loss_exact_marginalization_mean"]= pd.DataFrame(np.concatenate([loss_exact_marg_runs[i] for i in range(runs)], axis=1).mean(axis=-1), columns= exact_col, index= loss_exact_marg_runs[0].index)
-    exp_results["loss_appr_marginalization_mean"]= pd.DataFrame(np.stack([loss_appr_marg_runs[i] for i in range(runs)]).mean(axis=0), columns= appr_cols, index= loss_appr_marg_runs[0].index)
-    exp_results["loss_exact_marginalization_std"]= pd.DataFrame(np.concatenate([loss_exact_marg_runs[i] for i in range(runs)], axis=1).std(axis=-1), columns= exact_col, index= loss_exact_marg_runs[0].index)
-    exp_results["loss_appr_marginalization_std"]= pd.DataFrame(np.stack([loss_appr_marg_runs[i] for i in range(runs)]).std(axis=0), columns= appr_cols, index= loss_appr_marg_runs[0].index)
+stacked_results= {}
+for openml_id, temp_exp_results in exp_results.items():
+    n_train= temp_exp_results["experiment_hyperparameters"]["n_train"]
+    n_inf= temp_exp_results["experiment_hyperparameters"]["n_test"]
+
+    #Aggregate results of approximate marginalization
+    temp_stacked_results_appr_mean= temp_exp_results["loss_appr_marginalization_mean"].stack().reset_index()
+    temp_stacked_results_appr_mean.columns = ['M', 'L', 'Loss_Mean']
+
+    temp_stacked_results_appr_std= temp_exp_results["loss_appr_marginalization_std"].stack().reset_index()
+    temp_stacked_results_appr_std.columns = ['M', 'L', 'Loss_Std']
+
+    temp_stacked_results_appr= pd.merge(temp_stacked_results_appr_mean, temp_stacked_results_appr_std, on=['M', 'L'], how='left')
+    temp_stacked_results_appr["Approximate"]= True
+
+    #Compute the amount of token connections for approximate marginalization
+    temp_stacked_results_appr["Token_Connections"]= (n_train ** 2) + (n_train * n_inf *  temp_stacked_results_appr["M"] * temp_stacked_results_appr["L"])
 
 
-def plot_results_M_L_Loss(loss_appr_marg,
-                 loss_exact_marg):
-    #TODO: Integrate weights
+    #Aggregate results of exact marginalization
+    temp_stacked_results_exact_mean= temp_exp_results["loss_exact_marginalization_mean"].stack().reset_index()
+    temp_stacked_results_exact_mean.columns = ['M', 'L', 'Loss_Mean']
 
-    plot_df= loss_appr_marg.copy()
-    plot_df[-1]= loss_exact_marg.copy()
+    temp_stacked_results_exact_std= temp_exp_results["loss_exact_marginalization_std"].stack().reset_index()
+    temp_stacked_results_exact_std.columns = ['M', 'L', 'Loss_Std']
 
-    x, y = np.meshgrid(plot_df.columns, plot_df.index)
-    x_flat = x.flatten()
-    y_flat = y.flatten()
-    values = plot_df.values.flatten()
+    temp_stacked_results_exact= pd.merge(temp_stacked_results_exact_mean, temp_stacked_results_exact_std, on=['M', 'L'], how='left')
+    temp_stacked_results_exact["Approximate"]= False
 
-    plt.figure(figsize=(10, 6))
+    #Compute the amount of token connections for exact marginalization
+    temp_stacked_results_exact["Token_Connections"]= ((n_train ** 2) * temp_stacked_results_appr["M"]) + (n_train * n_inf * temp_stacked_results_appr["M"])
 
-    cmap_original = plt.get_cmap('magma')
-    cmap_inverted = ListedColormap(cmap_original.colors[::-1])
+    #Combine results
+    temp_stacked_results= pd.concat([temp_stacked_results_appr, temp_stacked_results_exact], axis= 0, ignore_index= True)
 
-    scatter = plt.scatter(x_flat, y_flat, c=values, cmap=cmap_inverted, s=30)
-    plt.colorbar(scatter, label='Values')
-    plt.title('Loss per M and L')
-    plt.xlabel('L')
-    plt.ylabel('M')
-    plt.show()
+    stacked_results[openml_id]= [temp_stacked_results, temp_stacked_results_exact, temp_stacked_results_appr]
 
-    del plot_df
+def plot_loss_per_tc(stacked_results, save):
+    fig, axs = plt.subplots(1, 3, figsize=(12, 4))
+    quantiles= [0.97, 0.95, 0.9999]
+    i=0
 
-# plot_each_run= False
+    for openml_id, temp_results in stacked_results.items():
+        temp_stacked_results, temp_stacked_results_exact, temp_stacked_results_appr= temp_results
 
-# # #Plot overall results
-# plot_results_M_L_Perf(exp_results["loss_appr_marginalization_mean"],
-#              exp_results["loss_exact_marginalization_mean"])
+        axs[i].errorbar(x=temp_stacked_results_exact['Token_Connections'], 
+                    y=temp_stacked_results_exact['Loss_Mean'], 
+                    yerr=temp_stacked_results_exact['Loss_Std'], 
+                    fmt='none', 
+                    capsize=0.5,
+                    elinewidth=1,
+                    marker='_', 
+                    color=cmap_inverted.colors[100], 
+                    alpha=0.2)
+        
+        axs[i].scatter(temp_stacked_results_exact['Token_Connections'],
+                    temp_stacked_results_exact['Loss_Mean'],
+                    s= 2,
+                    color= cmap_inverted.colors[100],
+                    label= "Exact")
+        
 
-# #Plot results per run
-# if plot_each_run:
-#     for run in range(runs):
-#         plot_results_M_L_Perf(loss_appr_marg_runs[run],
-#                      loss_exact_marg_runs[run])
+        
+        axs[i].errorbar(x=temp_stacked_results_appr['Token_Connections'], 
+                    y=temp_stacked_results_appr['Loss_Mean'], 
+                    yerr=temp_stacked_results_appr['Loss_Std'], 
+                    fmt='none', 
+                    capsize=0.5, 
+                    elinewidth=1, 
+                    marker='_', 
+                    color=cmap_inverted.colors[230], 
+                    alpha=0.05)
+
+        axs[i].scatter(temp_stacked_results_appr['Token_Connections'],
+                    temp_stacked_results_appr['Loss_Mean'],
+                    s= 0.5,
+                    color= cmap_inverted.colors[230],
+                    label= "Approximate")
 
 
-
-
-n_train= exp_results["experiment_hyperparameters"]["n_train"]
-n_inf= exp_results["experiment_hyperparameters"]["n_test"]
-
-#Aggregate results of approximate marginalization
-stacked_results_appr_mean= exp_results["loss_appr_marginalization_mean"].stack().reset_index()
-stacked_results_appr_mean.columns = ['M', 'L', 'Loss_Mean']
-
-stacked_results_appr_std= exp_results["loss_appr_marginalization_std"].stack().reset_index()
-stacked_results_appr_std.columns = ['M', 'L', 'Loss_Std']
-
-stacked_results_appr= pd.merge(stacked_results_appr_mean, stacked_results_appr_std, on=['M', 'L'], how='left')
-stacked_results_appr["Approximate"]= True
-
-#Compute the amount of token connections for approximate marginalization
-stacked_results_appr["Token_Connections"]= (n_train ** 2) + (n_train * n_inf *  stacked_results_appr["M"] * stacked_results_appr["L"])
-
-
-#Aggregate results of exact marginalization
-stacked_results_exact_mean= exp_results["loss_exact_marginalization_mean"].stack().reset_index()
-stacked_results_exact_mean.columns = ['M', 'L', 'Loss_Mean']
-
-stacked_results_exact_std= exp_results["loss_exact_marginalization_std"].stack().reset_index()
-stacked_results_exact_std.columns = ['M', 'L', 'Loss_Std']
-
-stacked_results_exact= pd.merge(stacked_results_exact_mean, stacked_results_exact_std, on=['M', 'L'], how='left')
-stacked_results_exact["Approximate"]= False
-
-#Compute the amount of token connections for exact marginalization
-stacked_results_exact["Token_Connections"]= ((n_train ** 2) * stacked_results_appr["M"]) + (n_train * n_inf * stacked_results_appr["M"])
-
-#Combine results
-stacked_results= pd.concat([stacked_results_appr, stacked_results_exact], axis= 0, ignore_index= True)
-
-# plot_results_Conn_Perf()
-
-def plot_results_TokenCons_Loss(exact_results,
-                                 appr_results):
-    plt.figure(figsize=(10, 6))
-
-    # cmap_original = plt.get_cmap('magma')
-    # cmap_inverted = ListedColormap(cmap_original.colors[::-1])
-
-    plt.errorbar(x=exact_results['Token_Connections'], 
-                y=exact_results['Loss_Mean'], 
-                yerr=exact_results['Loss_Std'], 
-                fmt='o', 
-                linestyle='None', 
-                capsize=0.1, 
-                label= "Exact")
+        axs[i].set_title(f'{str(openml.datasets.get_dataset(openml_id).name)} dataset')
+        axs[i].set_xlabel('Token connections')
+        axs[i].set_ylabel('Loss')
+        axs[i].set_ylim(0, (temp_stacked_results['Loss_Mean'] + temp_stacked_results['Loss_Std']).quantile(quantiles[i]))
     
-    plt.errorbar(x=appr_results['Token_Connections'], 
-                y=appr_results['Loss_Mean'], 
-                yerr=appr_results['Loss_Std'], 
-                fmt='o', 
-                linestyle='None', 
-                capsize=0.01, 
-                label= "Approximate")
-    
-    plt.title('Loss per Token Connections')
-    plt.xlabel('Token Connections')
-    plt.ylabel('Loss')
-    plt.ylim(0, stacked_results['Loss_Mean'].max())
-    plt.show()
+        axs[i].set_xscale('log')
 
-plot_results_TokenCons_Loss(stacked_results_exact, stacked_results_appr)
+        i+=1
+
+    plt.tight_layout()
+    plt.legend()
+
+    if save:
+        plt.savefig('experiments/kernel_shap/results/Loss_TC.pdf', bbox_inches='tight')  
+    else:
+        plt.show()
+
+      
+
+plot_loss_per_tc(stacked_results, False)
