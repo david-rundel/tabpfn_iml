35c9b7bd5538942ab762af82b10bcdfbd13b3245
davidrundel <david.rundel@campus.lmu.de>
2024-03-11 16:03:38 +0100

    pep8

--- experiments/ice_pd/analyze_results_ice_pd.py
+++ experiments/ice_pd/analyze_results_ice_pd.py
@@ -8,78 +8,88 @@ import os
 cmap_original = plt.get_cmap('magma')
 cmap_inverted = ListedColormap(cmap_original.colors[::-1])
 
+
 def plot_barplot(df, x, y, hue, hue_order, title, palette):
     plt.figure()
     sns.set_theme(style="ticks", font='sans-serif', font_scale=1.5)
     sorted_df = df.sort_values(by=y, ascending=True)
     sns.barplot(data=sorted_df,
-                    x=x, y=y, hue=hue, hue_order=hue_order, palette=palette, saturation=1.0)
+                x=x, y=y, hue=hue, hue_order=hue_order, palette=palette, saturation=1.0)
     plt.xticks(rotation=45)
     plt.title(title)
     plt.tight_layout()
     return plt
 
-def get_latest_csv_file(directory:str,
-                        prefix:str,
-                        criterion:str=None):
+
+def get_latest_csv_file(directory: str,
+                        prefix: str,
+                        criterion: str = None):
     files = os.listdir(directory)
     if criterion is not None:
-        filtered_files = [file for file in files if file.startswith(prefix) and criterion not in file]
+        filtered_files = [file for file in files if file.startswith(
+            prefix) and criterion not in file]
     else:
         filtered_files = [file for file in files if file.startswith(prefix)]
     sorted_files = sorted(filtered_files, reverse=True)
     latest_file = sorted_files[0]
     return pd.read_csv(f'{directory}/{latest_file}')
 
-df_pdp = get_latest_csv_file('experiments/ice_pd/results', 'benchmark_results_pdp_', 'openml')
+
+df_pdp = get_latest_csv_file(
+    'experiments/ice_pd/results', 'benchmark_results_pdp_', 'openml')
 
 barplot_pdp = plot_barplot(df=df_pdp,
-                       x="Number of unique Feature Values",
-                       y="Runtime in Seconds",
-                       hue="Implementation",
-                       hue_order=["tabpfn_iml PDP", "scikit-learn PDP"],
-                       title="PDP Runtime Comparison",
-                       palette={"tabpfn_iml PDP": cmap_inverted.colors[100], "scikit-learn PDP": cmap_inverted.colors[230]})
+                           x="Number of unique Feature Values",
+                           y="Runtime in Seconds",
+                           hue="Implementation",
+                           hue_order=["tabpfn_iml PDP", "scikit-learn PDP"],
+                           title="PDP Runtime Comparison",
+                           palette={"tabpfn_iml PDP": cmap_inverted.colors[100], "scikit-learn PDP": cmap_inverted.colors[230]})
 
 
 barplot_pdp.savefig('experiments/ice_pd/pdp_runtime_comparison_synth.pdf')
 
 
-df_ale = get_latest_csv_file('experiments/ice_pd/results', 'benchmark_results_ale_', 'openml')
+df_ale = get_latest_csv_file(
+    'experiments/ice_pd/results', 'benchmark_results_ale_', 'openml')
 
 barplot_ale = plot_barplot(df=df_ale,
-                       x="Number of unique Feature Values",
-                       y="Runtime in Seconds",
-                       hue="Implementation",
-                       hue_order=["tabpfn_iml ALE", "PyALE ALE"],
-                       title="PDP Runtime Comparison",
-                       palette={"tabpfn_iml ALE": cmap_inverted.colors[100], "PyALE ALE": cmap_inverted.colors[230]})
+                           x="Number of unique Feature Values",
+                           y="Runtime in Seconds",
+                           hue="Implementation",
+                           hue_order=["tabpfn_iml ALE", "PyALE ALE"],
+                           title="PDP Runtime Comparison",
+                           palette={"tabpfn_iml ALE": cmap_inverted.colors[100], "PyALE ALE": cmap_inverted.colors[230]})
 
 barplot_ale.savefig('experiments/ice_pd/ale_runtime_comparison_synth.pdf')
 
 
-df_pdp_openml = get_latest_csv_file('experiments/ice_pd/results', 'benchmark_results_pdp_openml_')
+df_pdp_openml = get_latest_csv_file(
+    'experiments/ice_pd/results', 'benchmark_results_pdp_openml_')
 
 barplot_pdp_openml = plot_barplot(df=df_pdp_openml,
-                          x="Number of unique Feature Values",
-                          y="Runtime in Seconds",
-                          hue="Implementation",
-                          hue_order=["tabpfn_iml PDP", "scikit-learn PDP"],
-                          title="PDP Runtime Comparison",
-                          palette={"tabpfn_iml PDP": cmap_inverted.colors[100], "scikit-learn PDP": cmap_inverted.colors[230]})
+                                  x="Number of unique Feature Values",
+                                  y="Runtime in Seconds",
+                                  hue="Implementation",
+                                  hue_order=["tabpfn_iml PDP",
+                                             "scikit-learn PDP"],
+                                  title="PDP Runtime Comparison",
+                                  palette={"tabpfn_iml PDP": cmap_inverted.colors[100], "scikit-learn PDP": cmap_inverted.colors[230]})
 
-barplot_pdp_openml.savefig('experiments/ice_pd/pdp_runtime_comparison_openml.pdf')
+barplot_pdp_openml.savefig(
+    'experiments/ice_pd/pdp_runtime_comparison_openml.pdf')
 
 
-df_ale_openml = get_latest_csv_file('experiments/ice_pd/results', 'benchmark_results_ale_openml_')
+df_ale_openml = get_latest_csv_file(
+    'experiments/ice_pd/results', 'benchmark_results_ale_openml_')
 
 barplot_ale_openml = plot_barplot(df=df_ale_openml,
-                            x="Number of unique Feature Values",
-                            y="Runtime in Seconds",
-                            hue="Implementation",
-                            hue_order=["tabpfn_iml ALE", "PyALE ALE"],
-                            title="ALE Runtime Comparison",
-                            palette={"tabpfn_iml ALE": cmap_inverted.colors[100], "PyALE ALE": cmap_inverted.colors[230]})
+                                  x="Number of unique Feature Values",
+                                  y="Runtime in Seconds",
+                                  hue="Implementation",
+                                  hue_order=["tabpfn_iml ALE", "PyALE ALE"],
+                                  title="ALE Runtime Comparison",
+                                  palette={"tabpfn_iml ALE": cmap_inverted.colors[100], "PyALE ALE": cmap_inverted.colors[230]})
 
-barplot_ale_openml.savefig('experiments/ice_pd/ale_runtime_comparison_openml.pdf')
-
+barplot_ale_openml.savefig(
+    'experiments/ice_pd/ale_runtime_comparison_openml.pdf')
--- experiments/ice_pd/runtime_benchmark.py
+++ experiments/ice_pd/runtime_benchmark.py
@@ -39,10 +39,12 @@ RUNS = 5
 
 SEED = 42
 
-seed_shift = 0 # Default 0. Can be used to proceed an experiment where there have already been seed_shift-many runs with different seeds.
+# Default 0. Can be used to proceed an experiment where there have already been seed_shift-many runs with different seeds.
+seed_shift = 0
 
 np.random.seed(42)
-seeds = [np.random.randint(1, 10000) for _ in range(seed_shift + RUNS)][seed_shift:seed_shift+RUNS]
+seeds = [np.random.randint(1, 10000) for _ in range(
+    seed_shift + RUNS)][seed_shift:seed_shift+RUNS]
 
 if torch.backends.mps.is_available():
     DEVICE = 'mps'
@@ -63,19 +65,24 @@ def generate_classification_dataset(seed: int,
                                     feature: int = 0):
     np.random.seed(seed)
     X = np.random.rand(n, p)
-    
+
     noise = np.random.normal(0, 0.1, size=n)
-    nonlinear_sum = np.sum(np.sin(X), axis=1) + np.sum(np.exp(X), axis=1) - np.sum(np.log(X + 1), axis=1)
+    nonlinear_sum = np.sum(np.sin(X), axis=1) + \
+        np.sum(np.exp(X), axis=1) - np.sum(np.log(X + 1), axis=1)
     y_continuous = nonlinear_sum + noise
 
     # Disretize feature of interest, e.g., feature 0, into 'levels' classes:
-    discretizer_x = KBinsDiscretizer(n_bins=levels, encode='ordinal', strategy='quantile')
-    X[:, feature] = discretizer_x.fit_transform(X[:, feature].reshape(-1, 1)).flatten()
-    
+    discretizer_x = KBinsDiscretizer(
+        n_bins=levels, encode='ordinal', strategy='quantile')
+    X[:, feature] = discretizer_x.fit_transform(
+        X[:, feature].reshape(-1, 1)).flatten()
+
     # Discretize y_continuous into 2 classes (binary task)
-    discretizer = KBinsDiscretizer(n_bins=2, encode='ordinal', strategy='uniform')
-    y = discretizer.fit_transform(y_continuous.reshape(-1, 1)).flatten().astype(int)
-    
+    discretizer = KBinsDiscretizer(
+        n_bins=2, encode='ordinal', strategy='uniform')
+    y = discretizer.fit_transform(
+        y_continuous.reshape(-1, 1)).flatten().astype(int)
+
     return X, y
 
 
@@ -86,7 +93,7 @@ def benchmark_implementations(X_train: np.ndarray,
                               seed: int,
                               level: int,
                               n_train: int,
-                              n_test: int ):
+                              n_test: int):
     # Initialize classifier
     time_init_start = time.time()
     clf = TabPFNClassifier(seed=seed)
@@ -96,7 +103,7 @@ def benchmark_implementations(X_train: np.ndarray,
     print("Scikit-learn PDP...")
     start_time_pdp = time.time()
     partial_dependence(clf,
-                       X_train[:n_train,:],
+                       X_train[:n_train, :],
                        features=[feature],
                        grid_resolution=level,
                        percentiles=(0, 1),
@@ -106,7 +113,7 @@ def benchmark_implementations(X_train: np.ndarray,
 
     print("PyALE ALE...")
     start_time_ale = time.time()
-    ale(X=pd.DataFrame(X_train[:n_train,:]),
+    ale(X=pd.DataFrame(X_train[:n_train, :]),
         model=clf,
         feature=[feature],
         grid_size=level,
@@ -142,7 +149,7 @@ def benchmark_implementations(X_train: np.ndarray,
 
 
 # benchmark loop
-def run_benchmark(feature_id_of_interest:int, openml: bool = False, open_cc_dids=None, level_list=None):
+def run_benchmark(feature_id_of_interest: int, openml: bool = False, open_cc_dids=None, level_list=None):
     if openml:
         results = []
         for did in open_cc_dids:
@@ -150,18 +157,19 @@ def run_benchmark(feature_id_of_interest:int, openml: bool = False, open_cc_dids
                 print("=" * 50)
                 print(f"Benchmarking dataset {did} and seed {current_seed}...")
                 oml_temp = OpenMLData(did)
-                print(f"Categorical feature indices: {oml_temp.categorical_features_idx}")
+                print(
+                    f"Categorical feature indices: {oml_temp.categorical_features_idx}")
                 n_train = round(0.8*oml_temp.X.shape[0])
                 n_test = round(0.2*oml_temp.X.shape[0])
                 # The following line does not make sense, for ALE, the comp. complexity depends on the number of feature intervals, not the number of unique feature values.
                 sklearn_runtime_pdp, tabpfniml_runtime_pdp, pyale_runtime_ale, tabpfniml_runtime_ale = benchmark_implementations(X_train=oml_temp.X,
-                                                                                                                                y_train=oml_temp.y,
-                                                                                                                                oml=oml_temp,
-                                                                                                                                feature=feature_id_of_interest,
-                                                                                                                                level=100, 
-                                                                                                                                n_train=n_train,
-                                                                                                                                n_test=n_test,
-                                                                                                                                seed=current_seed)
+                                                                                                                                 y_train=oml_temp.y,
+                                                                                                                                 oml=oml_temp,
+                                                                                                                                 feature=feature_id_of_interest,
+                                                                                                                                 level=100,
+                                                                                                                                 n_train=n_train,
+                                                                                                                                 n_test=n_test,
+                                                                                                                                 seed=current_seed)
                 results.append({
                     "dataset": did,
                     "scikit-learn PDP": sklearn_runtime_pdp,
@@ -177,21 +185,24 @@ def run_benchmark(feature_id_of_interest:int, openml: bool = False, open_cc_dids
         for level in level_list:
             for current_seed in seeds:
                 print("=" * 50)
-                print(f"Benchmarking dataset with levels {level} and seed {current_seed}...")
-                X_train, y_train = generate_classification_dataset(levels=level, feature=feature_id_of_interest, seed=current_seed)
-                oml_temp = ArrayData(str(level), X_train, y_train, categorical_features_idx=[], feature_names=[f'x{i}' for i in range(X_train.shape[1])])
+                print(
+                    f"Benchmarking dataset with levels {level} and seed {current_seed}...")
+                X_train, y_train = generate_classification_dataset(
+                    levels=level, feature=feature_id_of_interest, seed=current_seed)
+                oml_temp = ArrayData(str(level), X_train, y_train, categorical_features_idx=[
+                ], feature_names=[f'x{i}' for i in range(X_train.shape[1])])
                 X_train = oml_temp.X
                 print(X_train.shape)
                 y_train = oml_temp.y
                 print(y_train.shape)
                 sklearn_runtime_pdp, tabpfniml_runtime_pdp, pyale_runtime_ale, tabpfniml_runtime_ale = benchmark_implementations(X_train=X_train,
-                                                                                                                                y_train=y_train,
-                                                                                                                                oml=oml_temp,
-                                                                                                                                feature=feature_id_of_interest,
-                                                                                                                                level=level,
-                                                                                                                                n_train=N_TRAIN,
-                                                                                                                                n_test=N_TEST,
-                                                                                                                                seed=current_seed)
+                                                                                                                                 y_train=y_train,
+                                                                                                                                 oml=oml_temp,
+                                                                                                                                 feature=feature_id_of_interest,
+                                                                                                                                 level=level,
+                                                                                                                                 n_train=N_TRAIN,
+                                                                                                                                 n_test=N_TEST,
+                                                                                                                                 seed=current_seed)
                 results.append({
                     "dataset": level,
                     "scikit-learn PDP": sklearn_runtime_pdp,
@@ -204,28 +215,36 @@ def run_benchmark(feature_id_of_interest:int, openml: bool = False, open_cc_dids
 
     results_df = pd.DataFrame(results)
 
-    results_df_pdp = results_df[["dataset", "Number of unique Featurevalues", "scikit-learn PDP", "tabpfn_iml PDP", "seed"]]
-    results_df_ale = results_df[["dataset", "Number of unique Featurevalues", "PyALE ALE", "tabpfn_iml ALE", "seed"]]
+    results_df_pdp = results_df[[
+        "dataset", "Number of unique Featurevalues", "scikit-learn PDP", "tabpfn_iml PDP", "seed"]]
+    results_df_ale = results_df[[
+        "dataset", "Number of unique Featurevalues", "PyALE ALE", "tabpfn_iml ALE", "seed"]]
     melted_df_pdp = results_df_pdp.melt(id_vars=["dataset", "Number of unique Featurevalues", "seed"],
-                            var_name="Implementation", value_name="Runtime in Seconds")
+                                        var_name="Implementation", value_name="Runtime in Seconds")
     melted_df_ale = results_df_ale.melt(id_vars=["dataset", "Number of unique Featurevalues", "seed"],
-                        var_name="Implementation", value_name="Runtime in Seconds")
-    
+                                        var_name="Implementation", value_name="Runtime in Seconds")
+
     return melted_df_pdp, melted_df_ale
 
 
 melted_df_pdp, melted_df_ale = run_benchmark(openml=False,
-                                             level_list=[5, 10, 25, 50, 100, 200, 300],
+                                             level_list=[5, 10, 25,
+                                                         50, 100, 200, 300],
                                              feature_id_of_interest=1)
 current_time = datetime.datetime.now().strftime("%Y_%m_%d_%H%M")
-melted_df_pdp.to_csv(f"experiments/ice_pd/results/benchmark_results_pdp_{current_time}.csv")
-melted_df_ale.to_csv(f"experiments/ice_pd/results/benchmark_results_ale_{current_time}.csv")
+melted_df_pdp.to_csv(
+    f"experiments/ice_pd/results/benchmark_results_pdp_{current_time}.csv")
+melted_df_ale.to_csv(
+    f"experiments/ice_pd/results/benchmark_results_ale_{current_time}.csv")
 print("Benchmarking synthetic datasets done.")
 
 melted_df_pdp, melted_df_ale = run_benchmark(openml=True,
-                                             open_cc_dids=[11, 15, 31, 1049, 37, 40982, 1494],
+                                             open_cc_dids=[
+                                                 11, 15, 31, 1049, 37, 40982, 1494],
                                              feature_id_of_interest=1)
 current_time = datetime.datetime.now().strftime("%Y_%m_%d_%H%M")
-melted_df_pdp.to_csv(f"experiments/ice_pd/results/benchmark_results_pdp_openml_{current_time}.csv")
-melted_df_ale.to_csv(f"experiments/ice_pd/results/benchmark_results_ale_openml_{current_time}.csv")
+melted_df_pdp.to_csv(
+    f"experiments/ice_pd/results/benchmark_results_pdp_openml_{current_time}.csv")
+melted_df_ale.to_csv(
+    f"experiments/ice_pd/results/benchmark_results_ale_openml_{current_time}.csv")
 print("Benchmarking openml datasets done.")
